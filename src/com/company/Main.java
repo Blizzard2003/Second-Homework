8c75eab63e08b7526f236a7fcd0c6acaeffa152e
unknown <GITHUB andyskl03@gmail.com>
2018-09-15 00:27:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*int year;
+        Scanner in = new Scanner(System.in);
+        System.out.println("В каком году была основана Одесса?");
+        year = in.nextInt();
+        if (year == 1794) {
+            System.out.println("Вы правы");
+        } else {
+            System.out.println("ВЫ не правы, Одесса была основана в 1794");*/
+
+        /*int a;
+        int b;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите делимое: ");
+        a = in.nextInt();
+        System.out.println("Введите делитель: ");
+        b = in.nextInt();
+        System.out.println("Результат: ");
+        System.out.println(a/b );*/
+
+        /*double suma1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите стоимость покупки от 500 до 1000: ");
+        suma1 = in.nextDouble();
+        if (suma1 >= 500 && suma1 < 1000){
+            System.out.println("Стоимость вашей покупки со скидкой: ");
+            System.out.println(suma1 - suma1*0.03);
+        }
+        if (suma1 >= 1000){
+            System.out.println("Стоимость вашей покупки со скидкой:");
+            System.out.println(suma1 - suma1*0.05);
+
+
+        }*/
+
+        /*int cheslo;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        cheslo = in.nextInt();
+        if ((cheslo % 2) == 0) {
+            System.out.println("Число " + cheslo + " четное ");
+        } else {
+            if ((cheslo % 1) == 0) {
+                System.out.println("Число " + cheslo + " нечетное ");
+            }
+        }*/
+
+    }
+
+}
+
+
+
+
+
+
